2de8f26fe88ac1e435ad54296b306a06fb0c261b
jin-yeong-kim <khy2240@gmail.com>
2023-02-17 16:54:00 +0900

    {김진영} war 파일 푸쉬

--- server/preproject/src/main/java/com/codestates/PreprojectApplication.java
+++ server/preproject/src/main/java/com/codestates/PreprojectApplication.java
@@ -2,12 +2,18 @@ package com.codestates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PreprojectApplication {
+public class PreprojectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PreprojectApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PreprojectApplication.class);
+	}
 }
