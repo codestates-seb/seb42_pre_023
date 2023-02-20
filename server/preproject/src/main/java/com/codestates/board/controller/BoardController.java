c09916a2c386f19f2f0eafb8ed291a0db0cb8dd3
jin-yeong-kim <khy2240@gmail.com>
2023-02-20 17:04:07 +0900

    보드 컨트롤러 스텁 데이터 적용 및 보드 DTO

--- server/preproject/src/main/java/com/codestates/board/controller/BoardController.java
+++ server/preproject/src/main/java/com/codestates/board/controller/BoardController.java
@@ -1,5 +1,45 @@
 package com.codestates.board.controller;
 
+import com.codestates.board.dto.BoardDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.util.Arrays;
+
+@RequestMapping("/boards")
 public class BoardController {
+
+    @PostMapping
+    public ResponseEntity postBoard() {
+        return ResponseEntity.created(URI.create("/boards/1")).build();
+    }
+
+    @PatchMapping("/{board-id}")
+    public ResponseEntity patchBoard() {
+        BoardDto.Response response =
+                new BoardDto.Response(1,1, "커밋이 안돼요", "커밋이 안됩니다.", 0, 0, 0, "2023-11-22 12:32:32.433");
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{board-id}")
+    public ResponseEntity getBoard() {
+        BoardDto.Response response =
+                new BoardDto.Response(1,1, "커밋이 안돼요", "커밋이 안됩니다.", 0, 0, 0, "2023-11-22 12:32:32.433");
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping
+    public ResponseEntity getBoards() {
+        BoardDto.Response response1 =
+                new BoardDto.Response(1,1, "커밋이 안돼요", "커밋이 안됩니다.", 0, 0, 0, "2023-11-22 12:32:32.433");
+        BoardDto.Response response2 =
+                new BoardDto.Response(1,1, "커밋이 안돼요", "커밋이 안됩니다.", 0, 0, 0, "2023-11-22 12:32:32.433");
+        return ResponseEntity.ok(Arrays.asList(response1, response2));
+    }
+
+    @DeleteMapping("/{board-id}")
+    public ResponseEntity deleteBoard() {
+        return ResponseEntity.noContent().build();
+    }
 }
