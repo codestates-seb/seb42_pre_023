1cc5f46d6beb48c46a7302af37c894d356bae7b6
hunjeong <rhkrgnswjd@naver.com>
2023-02-21 17:25:19 +0900

    feat: UriCreator 적용

--- server/preproject/src/main/java/com/codestates/member/controller/MemberControllerV1.java
+++ server/preproject/src/main/java/com/codestates/member/controller/MemberControllerV1.java
@@ -4,6 +4,7 @@ import com.codestates.member.dto.MemberDto;
 import com.codestates.member.entity.Member;
 import com.codestates.member.mapper.MemberMapper;
 import com.codestates.member.service.MemberService;
+import com.codestates.utils.UriCreator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -35,7 +36,7 @@ public class MemberControllerV1 {
         member.setMemberGrade(Member.MemberGrade.MEMBER_NOMAL);
 
         Member createdMember = memberService.createMember(member);
-        URI location = UriCreator.createUri(MEMBER_DEFAULT_URL);
+        URI location = UriCreator.createUri(MEMBER_DEFAULT_URL, createdMember.getMemberId());
 
         return ResponseEntity.created(location).build();
     }
