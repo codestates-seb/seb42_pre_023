91374501fff0352e663d155cc2ca6aeee57705d1
RaYul18 <prettykiki92@gmail.com>
2023-02-22 16:09:16 +0900

    Comment CRUD 작성완/테스트전

--- server/preproject/src/main/java/com/codestates/comment/repository/CommentRepository.java
+++ server/preproject/src/main/java/com/codestates/comment/repository/CommentRepository.java
@@ -3,5 +3,9 @@ package com.codestates.comment.repository;
 import com.codestates.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Optional<Comment> findByBoardId(Long boardId);
+
 }
