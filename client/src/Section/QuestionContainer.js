1c118febd37b1522310779a325400da9b6850c62
sienna0715 <5609373@naver.com>
2023-03-02 18:19:36 +0900

    feat: 배포 에러 수정 9차

--- client/src/Section/QuestionContainer.js
+++ client/src/Section/QuestionContainer.js
@@ -71,13 +71,13 @@ function QuestionContainer({ questionList }) {
   return (
     <Container>
       <Question>
-        {questionList
+        {questionList ? questionList
           .slice(items * (page - 1), items * (page - 1) + items)
           .map((questions) => {
             return (
               <QuestionCard key={questions.boardId} questions={questions} />
             );
-          })}
+          }) : <></>}
       </Question>
       <PaginationBox>
         <Pagination
