9385271aaa0ccd6f2559a218aa7060456d6dc867
jin-yeong-kim <khy2240@gmail.com>
2023-03-22 22:17:48 +0900

    S

--- server/preproject/src/main/java/com/codestates/board/dto/BoardPatchDto.java
+++ server/preproject/src/main/java/com/codestates/board/dto/BoardPatchDto.java
@@ -1,11 +1,9 @@
 package com.codestates.board.dto;
 
-import com.codestates.board.entity.BoardTag;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Getter
